062da6c0351935a479399576e5065f64f93608ae
cjiaxin02 <cjiaxin02@gmail.com>
2026-05-03 01:16:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,11 @@ from supabase import create_client
 app = Flask(__name__)
 
 # 從環境變數讀取金鑰 (Render 設定區)
-line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.environ.get('CHANNEL_SECRET'))
-supabase_url = os.environ.get('SUPABASE_URL')
-supabase_key = os.environ.get('SUPABASE_KEY')
+# 這樣寫可以確保如果讀不到鑰匙，會給它一個空的字串 "" 而不是 None
+line_bot_api = LineBotApi(os.environ.get('CHANNEL_ACCESS_TOKEN', ''))
+handler = WebhookHandler(os.environ.get('CHANNEL_SECRET', ''))
+supabase_url = os.environ.get('SUPABASE_URL', '')
+supabase_key = os.environ.get('SUPABASE_KEY', '')
 supabase = create_client(supabase_url, supabase_key)
 
 @app.route("/callback", methods=['POST'])
